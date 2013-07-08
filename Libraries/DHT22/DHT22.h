d89f8ad97a294a531b9cb815692ec40d5bd5e92f
akhilvasvani <sonofhotmale@gmail.com>
2013-07-08 14:59:19 +0530

    Updated DHT22.h file for Ardunio 1.04 usage

--- Libraries/DHT22/DHT22.h
+++ Libraries/DHT22/DHT22.h
@@ -1,9 +1,9 @@
 // 
 //    FILE: dht.h
-// VERSION: 0.1.00
+// VERSION: 0.1.05
 // PURPOSE: DHT Temperature & Humidity Sensor library for Arduino
 //
-//     URL: http://arduino.cc/playground/Main/DHTLib
+//     URL: http://playground.arduino.cc/Main/DHTLib
 //
 // HISTORY:
 // see dht.cpp file
@@ -12,21 +12,29 @@
 #ifndef dht_h
 #define dht_h
 
-#include "WProgram.h"
+#if ARDUINO < 100
+#include <WProgram.h>
+#else
+#include <Arduino.h>
+#endif
 
-#define DHT_LIB_VERSION "0.1.00"
+#define DHT_LIB_VERSION "0.1.05"
+
+#define DHTLIB_OK                               0
+#define DHTLIB_ERROR_CHECKSUM   -1
+#define DHTLIB_ERROR_TIMEOUT    -2
+#define DHTLIB_INVALID_VALUE    -999
 
 class dht
 {
 public:
-	int read11(uint8_t pin);
     int read22(uint8_t pin);
-	double humidity;
-	double temperature;
+        double humidity;
+        double temperature;
 
 private:
-	uint8_t bits[5];  // buffer to receive data
-	int read(uint8_t pin);
+        uint8_t bits[5];  // buffer to receive data
+        int read(uint8_t pin);
 };
 #endif
 //
